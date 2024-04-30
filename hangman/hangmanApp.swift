a29fafe82d6c481d43821671bc760783c36efadd
Colton Mueller <mueller_colton25@yahoo.com>
2024-04-29 21:09:06 -0500

    Initial Commit

---
+++ hangman/hangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  hangmanApp.swift
+//  hangman
+//
+//  Created by Colton Mueller on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct hangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
